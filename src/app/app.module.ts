fedb22ac12af273e1a4ad4178a31bcb0f5039e05
Jorge <jorgearmas49@gmail.com>
2023-09-27 12:24:09 -0600

    HttpClientModule and FormsModule imported to NgModule dec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
@@ -12,7 +15,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
