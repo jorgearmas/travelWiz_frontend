bc5498ec4fcb7dae29a8c76df0ba26c2ead55636
Jorge <jorgearmas49@gmail.com>
2023-09-27 12:25:10 -0600

    View and Controller set for login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  usuario: any = {
+  }
+
+  constructor(private http: HttpClient){
+  }
+
+  login(){
+    let validarFormulario: any = document.getElementById("loginForm")
+
+    if (validarFormulario.reportValidity()){
+      this.servicioLogin().subscribe((u:any) => this.darBienvenida(u))
+    }
+  }
+
+  darBienvenida(usuario: any){
+    if(usuario){
+      location.href="/bienvenida"
+    }
+    else{
+      alert("Error en credenciales")
+    }
+  }
+
+  servicioLogin(){
+    let httpOptions = {
+      headers: new HttpHeaders({'Content-Type':'application/json'})
+    }
+
+  return this.http.post<any>("http://localhost:8080/usuario/guardar", this.usuario, httpOptions)
+  }
 }
+
