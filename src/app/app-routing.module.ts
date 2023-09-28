2a952bca64da4d764d21df51a93037ee40168190
Jorge <jorgearmas49@gmail.com>
2023-09-28 12:58:29 -0600

    Routes added for component lugar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { LugarComponent } from './lugar/lugar.component';
 
 const routes: Routes = [
   { path: '', component:LoginComponent },
   { path: 'bienvenida', component:BienvenidaComponent },
+  { path: 'lugar', component:LugarComponent}
 ];
 
 @NgModule({
