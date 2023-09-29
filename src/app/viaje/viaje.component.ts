8de4a2c22a242bb92a92230ad0c409616d620887
Jorge <jorgearmas49@gmail.com>
2023-09-29 17:36:44 -0600

    Initial Structure for component viaje

---
+++ src/app/viaje/viaje.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viaje',
+  templateUrl: './viaje.component.html',
+  styleUrls: ['./viaje.component.css']
+})
+export class ViajeComponent {
+
+}
