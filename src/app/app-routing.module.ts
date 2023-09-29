0acc821e84f2718690a22031b67432837e5ab6ec
Jorge <jorgearmas49@gmail.com>
2023-09-29 17:29:06 -0600

    Route for component viaje added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { BienvenidaComponent } from './bienvenida/bienvenida.component';
 import { LugarComponent } from './lugar/lugar.component';
+import { ViajeComponent } from './viaje/viaje.component';
 
 const routes: Routes = [
   { path: '', component:LoginComponent },
   { path: 'bienvenida', component:BienvenidaComponent },
-  { path: 'lugar', component:LugarComponent}
+  { path: 'lugar', component:LugarComponent},
+  { path: 'viaje', component:ViajeComponent}
 ];
 
 @NgModule({
