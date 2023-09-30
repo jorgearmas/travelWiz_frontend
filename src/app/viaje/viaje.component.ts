e7f8629ddc4b7e5f35431bdf0feb2efca010a7e9
Jorge <jorgearmas49@gmail.com>
2023-09-29 20:46:07 -0600

    CRUD completed for component in View and Controller

--- src/app/viaje/viaje.component.ts
+++ src/app/viaje/viaje.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-viaje',
@@ -6,5 +8,89 @@ import { Component } from '@angular/core';
   styleUrls: ['./viaje.component.css']
 })
 export class ViajeComponent {
+  viaje:any = {};
+  viajes: any = [];
+  estados: any = [];
+  lugares: any = [];
 
+  constructor(private htpp:HttpClient)
+  {
+    this.buscarViajes();
+    this.buscarEstados();
+    this.buscarLugares();
+  }
+
+  //GUARDAR VIAJE
+  guardarViaje(){
+    let formularioValido:any = document.getElementById("viajeForm");
+
+    if(formularioValido.reportValidity())
+    {
+      this.servicioGuardarViaje().subscribe((v:any) => this.actualizar(v))
+    }
+
+  }
+
+  servicioGuardarViaje(){
+    let httpOptions =
+    {
+      headers:new HttpHeaders({'Content-Type':'application/json'})
+    }
+
+    return this.htpp.post("http://localhost:8080/viaje/guardar", this.viaje, httpOptions);
+  }
+
+  //BUSCAR VIAJE
+  buscarViajes(){
+    this.servicioBuscarViajes().subscribe((v:any) => this.viajes = v)
+  }
+
+  servicioBuscarViajes():Observable<any>{
+    return this.htpp.get<any>("http://localhost:8080/viaje/buscar");
+  }
+
+  //BUSCAR ESTADO
+  buscarEstados(){
+    this.servicioBuscarEstados().subscribe((e:any)=>this.estados = e)
+  }
+
+  servicioBuscarEstados():Observable<any>{
+    return this.htpp.get<any>("http://localhost:8080/estado/buscar");
+  }
+
+  //BUSCAR LUGAR
+  buscarLugares(){
+    this.servicioBuscarLugares().subscribe((l:any)=>this.lugares = l)
+  }
+
+  servicioBuscarLugares():Observable<any>{
+    return this.htpp.get<any>("http://localhost:8080/lugar/buscar");
+  }
+
+  //ELIMINAR VIAJE
+  eliminarViaje(v:any){
+    this.servicioEliminarViaje(v).subscribe((v:any)=>this.actualizar(v))
+  }
+
+  servicioEliminarViaje(v:any):Observable<any>{
+    return this.htpp.delete<any>("http://localhost:8080/viaje/eliminar/"+v.idviaje);
+  }
+
+  //MODIFICAR VIAJE
+  modificarViaje(v:any)
+  {
+    this.viaje = v;
+  }
+
+  //ACTUALIZAR PAGINA
+  actualizar(l:any)
+  {
+    this.buscarViajes();
+    this.viaje = {};
+  }
+
+  //REGRESAR A BIENVENIDA
+  goToBienvenida(){
+    location.href="/bienvenida";
+  }
 }
