6f960f2a9c3de149b8b974021b205e6411a55d4e
Jorge <jorgearmas49@gmail.com>
2023-10-03 21:51:40 -0600

    Initial Structuree ro component consulta-usuarios}

---
+++ src/app/consulta-usuarios/consulta-usuarios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-usuarios',
+  templateUrl: './consulta-usuarios.component.html',
+  styleUrls: ['./consulta-usuarios.component.css']
+})
+export class ConsultaUsuariosComponent {
+
+}
