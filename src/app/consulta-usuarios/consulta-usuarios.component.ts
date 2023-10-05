3ba24648e58c4e5bfcb0de241e425c4756b4e07f
Jorge <jorgearmas49@gmail.com>
2023-10-04 19:27:26 -0600

    Component consulta-usuarios shows table usuario

--- src/app/consulta-usuarios/consulta-usuarios.component.ts
+++ src/app/consulta-usuarios/consulta-usuarios.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-consulta-usuarios',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./consulta-usuarios.component.css']
 })
 export class ConsultaUsuariosComponent {
+  usuarios: any = [];
 
+  constructor(private htpp:HttpClient)
+  {
+    this.buscarUsuarios();
+  }
+
+    //BUSCAR USUARIOS
+    buscarUsuarios(){
+      this.servicioBuscarUsuarios().subscribe((u:any) => this.usuarios = u)
+    }
+
+    servicioBuscarUsuarios():Observable<any>{
+      return this.htpp.get<any>("http://localhost:8080/usuario/buscar");
+    }
 }
