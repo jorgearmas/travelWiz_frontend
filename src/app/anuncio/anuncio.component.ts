149a8e9dfc7e6e8319f157563f02dd43896c53bb
Jorge <jorgearmas49@gmail.com>
2023-10-11 22:03:46 -0600

    Anuncios mngment completed (CRUD)

--- src/app/anuncio/anuncio.component.ts
+++ src/app/anuncio/anuncio.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-anuncio',
@@ -6,5 +8,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./anuncio.component.css']
 })
 export class AnuncioComponent {
+  anuncio:any = {};
+  anuncios: any = [];
 
+  constructor(private htpp:HttpClient)
+  {
+    this.buscarAnuncios();
+  }
+
+  //GUARDAR ANUNCIO
+  guardarAnuncio(){
+    let formularioValido:any = document.getElementById("anuncioForm");
+
+    if(formularioValido.reportValidity())
+    {
+      this.servicioguardarAnuncio().subscribe((a:any) => this.actualizar(a))
+    }
+
+  }
+
+  servicioguardarAnuncio(){
+    let httpOptions =
+    {
+      headers:new HttpHeaders({'Content-Type':'application/json'})
+    }
+
+    return this.htpp.post("http://localhost:8080/anuncio/guardar", this.anuncio, httpOptions);
+  }
+
+  //BUSCAR ANUNCIOS
+  buscarAnuncios(){
+    this.servicioBuscarAnuncios().subscribe((a:any) => this.anuncios = a)
+  }
+
+  servicioBuscarAnuncios():Observable<any>{
+    return this.htpp.get<any>("http://localhost:8080/anuncio/buscar");
+  }
+
+   //ELIMINAR ANUNCIOS
+   eliminarAnuncio(a:any){
+    this.servicioEliminarAnuncio(a).subscribe((a:any)=>this.actualizar(a))
+  }
+
+  servicioEliminarAnuncio(a:any):Observable<any>{
+    return this.htpp.delete<any>("http://localhost:8080/anuncio/eliminar/"+a.idanuncio);
+  }
+
+  //MODIFICAR ANUNCIO
+  modificarAnuncio(a:any)
+  {
+    this.anuncio = a;
+  }
+
+  //ACTUALIZAR PAGINA
+  actualizar(a:any)
+  {
+    this.buscarAnuncios();
+    this.anuncio = {};
+  }
+
+  //GESTOR DE IMAGENES
+  onImageChange(event: any){
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      this.anuncio.imagen = reader.result as string;
+    };
+
+    reader.readAsDataURL(file);
+  }
 }
