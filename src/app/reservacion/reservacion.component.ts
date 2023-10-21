0f4d146e979812724682093c372100e595c6ee30
Jorge <jorgearmas49@gmail.com>
2023-10-20 19:31:37 -0600

    List with reservations added to view

--- src/app/reservacion/reservacion.component.ts
+++ src/app/reservacion/reservacion.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-reservacion',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./reservacion.component.css']
 })
 export class ReservacionComponent {
+  reservaciones: any = [];
 
+  constructor(private htpp:HttpClient)
+  {
+    this.buscarReservaciones();
+  }
+
+    //BUSCAR  RESERVACIONES
+    buscarReservaciones(){
+      this.servicioBuscarReservaciones().subscribe((r:any) => this.reservaciones = r)
+    }
+
+    servicioBuscarReservaciones():Observable<any>{
+      return this.htpp.get<any>("http://localhost:8080/reservacion/buscar");
+    }
 }
