d3825e4ad6f8f19bf02fe98d023da21c63c22c67
Jorge <jorgearmas49@gmail.com>
2023-11-09 08:49:19 -0600

    Pipe renders transforms int to String

---
+++ src/app/tipoanuncio.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TipoanuncioPipe } from './tipoanuncio.pipe';
+
+describe('TipoanuncioPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TipoanuncioPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
